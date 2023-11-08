9b4663a1be6cb74f1356100b3b1408fefe5af810
Reynaldo Gonzalez <reynaldosergiogonzalez@gmail.com>
2023-11-07 20:19:15 -0600

    add working directory and xaxis formatting

--- spyceplot.py
+++ spyceplot.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
 import pandas as pd
 import matplotlib.ticker as mticker  
+import os
+
+path = os.getcwd()
 
 # Search .CIR file for .PRINT statements and store output filename and print variables
-project_path = r'C:\Users\rgonzalez\OneDrive - SWRI\Projects\spyce\sample\sample.cir'
+project_path =  path + '\\' + 'sample\\sample.cir'
 statement = '.PRINT'
-
-path = r'C:\Users\rgonzalez\OneDrive - SWRI\Projects\spyce\sample'
 
 def parse_cir(file_path):
 	linesWithPrint = []
@@ -37,14 +38,22 @@ def parse_cir(file_path):
 output_file, variables = parse_cir(project_path)
 
 # Read the output file
-sim_data = pd.read_csv(path + '\\' + output_file[:-1], delim_whitespace=True)
+sim_data = pd.read_csv(path + '\\' + '\\sample\\' + output_file[:-1], delim_whitespace=True)
+print('\n')
 print(sim_data)
+print('\n')
+
+run_time = sim_data.iloc[[-1]]
+run_time = run_time.values[0][0]
 
 # Variable names from output csv
 outVar = list(sim_data.columns.tolist())
 
 xx = sim_data.loc[:, 'TIME']
+x2 = xx.div(run_time.astype(float))
 yy = sim_data.loc[:, outVar[1]]
+
+print(x2)
 
 # xpoints = np.array([1, 8])
 # ypoints = np.array([3, 10])
@@ -54,14 +63,11 @@ plt.title("Plot 1 - " + outVar[1])
 plt.xlabel("Time")
 plt.ylabel("Voltage")
 
-plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%.001f s'))
+plt.gca().xaxis.set_major_formatter(mticker.FormatStrFormatter('%.3f us'))
 
-plt.plot(xx, yy)
+plt.plot(x2, yy)
+plt.grid(linestyle = '--', linewidth = 0.5)
+plt.show()
 
-# plt.show()
-
-# This changes the formatter.
-plt.gca().xaxis.set_major_formatter(plt.xx.DateFormatter("%H:%M:%S.%f"))
-
-# Redraw the plot.
-plt.draw()
+# # Redraw the plot.
+# plt.draw()
